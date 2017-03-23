e41bfbad49988095595ec0c511db9792acbee050
Tobiasz12 <tobiasz63@op.pl>
2017-03-23 20:00:18 +0100

    grunt test

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+	  	sass: {
+    	    options: {
+        		sourceMap: true
+       		},
+      		dist: {
+       	 		files: {
+          			'css/main.css': 'sass/main.sass'
+        		}
+      		}
+  	  	},
+
+	  	imagemin: {
+			dynamic: {
+				files: [{
+					expand: true,
+					cwd: 'images/',
+					src: ['**/*.{png,jpg,gif}'],
+					dest: 'images/build/'
+				}]
+			}
+  		}
+
+   		watch: {
+			scripts: {
+				files: ['sass/*.sass'],
+				tasks: ['sass'],
+				options: {
+    				spawn: false,
+					},
+			} 	
+		}
+  	});
+
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+   // Default task(s).
+
+  grunt.registerTask('default', ['watch','sass','imagemin']);
+};
+
